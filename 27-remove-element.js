5a009ec78365c6d6751177d5e93bd3d02c5af88d
Alvin Sewram <alvinsewram@Alvins-MacBook-Pro.local>
2021-10-19 10:55:54 -0400

    27

---
+++ 27-remove-element.js
@@ -0,0 +1,23 @@
+// Since it is impossible to change the length of the array in some languages,
+// you must instead have the result be placed in the first part of the array nums.
+// More formally, if there are k elements after removing the duplicates,
+// then the first k elements of nums should hold the final result.
+// It does not matter what you leave beyond the first k elements.
+
+// Return k after placing the final result in the first k slots of nums.
+
+// Do not allocate extra space for another array.
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(nums.length, 0, null);
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums;
+};
+
+console.log(removeElement([3, 2, 3, 4, 5, 1, 3], 3));
